4fb44c33ff8b54e13988b000a215dc03ae951594
Thomas Röggla <tom@lichtfragmente.com>
2023-04-11 04:20:26 +0200

    added bash script for running the container

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "USAGE: $0 dataset_dir model_dir"
+  exit 1
+fi
+
+docker run -v ${1}:/app/dataset/ -v ${2}:/app/model/ --shm-size=512M grandchallenge python test.py
+
+container_id=`docker ps -a -q |head -n1`
+docker cp ${container_id}:/app/results.csv results.csv
+docker rm $container_id
+
+cat results.csv
